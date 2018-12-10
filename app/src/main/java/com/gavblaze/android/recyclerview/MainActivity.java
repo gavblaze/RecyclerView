d8402beee39e177e21462b6576cdbf5887cddb9b
Gavin <gav.plant@gmail.com>
2018-12-10 22:50:56 +1300

    Initial commit

---
+++ app/src/main/java/com/gavblaze/android/recyclerview/MainActivity.java
@@ -0,0 +1,85 @@
+package com.gavblaze.android.recyclerview;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.LinkedList;
+
+public class MainActivity extends AppCompatActivity implements WordListAdapter.OnItemClickListener {
+    private final LinkedList<String> mWordList = new LinkedList<>();
+    private WordListAdapter mWordListAdapter;
+    private RecyclerView mRecyclerView;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
+
+        mWordListAdapter = new WordListAdapter(mWordList, this);
+        mRecyclerView.setAdapter(mWordListAdapter);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+
+        for (int i = 0; i <= 20; i++) {
+            mWordList.add("Word " + i);
+        }
+
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int listSize = mWordList.size();
+                mWordList.addLast("+ Word " + listSize);
+                mWordListAdapter.notifyDataSetChanged();
+                mRecyclerView.smoothScrollToPosition(listSize);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemClick(int position) {
+        String wordOfClicked = mWordList.get(position);
+        mWordList.set(position, "Clicked! " + wordOfClicked);
+        mWordListAdapter.notifyDataSetChanged();
+    }
+}
---
+++ app/src/main/java/com/gavblaze/android/recyclerview/WordListAdapter.java
@@ -0,0 +1,63 @@
+package com.gavblaze.android.recyclerview;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.LinkedList;
+
+public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.ViewHolder> {
+    private LinkedList<String> mLinkedList;
+    private OnItemClickListener mOnItemClickListener;
+
+
+    public WordListAdapter(LinkedList<String> list, OnItemClickListener listener) {
+        this.mOnItemClickListener = listener;
+
+        this.mLinkedList = list;
+    }
+
+    public interface OnItemClickListener {
+       void onItemClick(int position);
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.word_list_item, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+        String word = mLinkedList.get(position);
+        viewHolder.mWordTextView.setText(word);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mLinkedList.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView mWordTextView;
+
+        private ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mWordTextView = itemView.findViewById(R.id.word_list_item);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int positon = getLayoutPosition();
+            mOnItemClickListener.onItemClick(positon);
+
+        }
+    }
+}
